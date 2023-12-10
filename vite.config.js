41f6b4f76a6b33894a9d1f9ebdf4c4552caa4d3d
Nitish T <tnitish477@gmail.com>
2023-12-10 19:51:12 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Fitness-app/",
+  base: "/Fitness-app1/",
   plugins: [react()],
 })
